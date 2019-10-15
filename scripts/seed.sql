7fb0157c98b741c922463395540291375d7d5bfa
Matthew Duguay <mduguay@wayfair.com>
2019-10-15 13:53:38 -0400

    Better defaults for stock seed

--- scripts/seed.sql
+++ scripts/seed.sql
@@ -9,7 +9,7 @@ INSERT INTO trade(trader_id, company_id, shares, price) VALUES (1, 1, 100, 134.6
 INSERT INTO quote(company_id, price, stamp) VALUES (1, 11.34, NOW()), (2, 6.66, NOW());
 
 INSERT INTO stock(company_id, vol, minchange, maxchange) 
-VALUES (1, 0.02, 0.0, 1.0), (2, 0.03, 0.0, 1.2);
+VALUES (1, 0.0015, 0.0, 1.0), (2, 0.0030, 0.0, 1.2);
 
 INSERT INTO cash(trader_id, amount) VALUES (1, 150000);
 
